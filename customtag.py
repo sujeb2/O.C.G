c827d8cf6610f998e7b5838d0aaf14c64eae52df
songro_ <sdbb2q@gmail.com>
2023-04-28 16:44:01 +0900

    added toggle buttons
    added toggle buttons

--- customtag.py
+++ customtag.py
@@ -2,6 +2,19 @@ from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QRadioBu
 from PyQt5.QtGui import QIcon
 import os, sys;
 
+# accuracy
+TOGGLED_ACC = False
+TOGGLED_XACC = False
+
+# bpm
+TOGGLED_CRB = False
+TOGGLED_TB = False
+TOGGLED_RKPS = False
+
+# progrss
+TOGGLED_PRGS = False
+TOGGLED_STARTPRGS = False
+
 class Main(QWidget):
     def __init__(self):
         super().__init__()
@@ -22,21 +35,35 @@ class Main(QWidget):
 
     def setWidgets(self):
         try:
-            # button
+            # label
             self.mainLabel1 = QLabel("OverLayer", self)
             self.mainLabel2 = QLabel("CustomTag Generator", self)
             self.mainVersion = QLabel("v1.0", self)
+
+            # button
             self.saveBtn = QPushButton("저장하기", self)
             self.loadBtn = QPushButton("불러오기", self)
+
+            # toggle
+            module_acc = QCheckBox("정학도", self)
+            module_xacc = QCheckBox("절대 정확도", self)
+            module_progress = QCheckBox("진행도", self)
+            module_startprgs = QCheckBox("시작 진행도", self)
+            module_crb = QCheckBox("체감 BPM", self)
+            module_tilebpm = QCheckBox("타일 BPM", self)
+            module_reckps = QCheckBox("체감 KPS", self)
+
+            # group
+            self.groupTagSelectList = QGroupBox("태그 모듈 목록")
             
             # reset btn/label position
             self.mainLabel1.move(15, 20)
             self.mainLabel2.move(15, 50)
             self.mainVersion.move(340, 50)
+
             self.saveBtn.move(15, 90)
             self.loadBtn.move(95, 90)
             
-
             # font reset
             mainLabelFont1 = self.mainLabel1.font()
             mainLabelFont1.setPointSize(20)
@@ -61,12 +88,18 @@ class Main(QWidget):
     def saveFile(self):
         print("saving...")
         print("ERROR Occurred!\nidk why it happend. sry about that :(")
-        errSaveFile = QMessageBox.critical(self, '오류가 발생하였습니다.', '파일을 저장하는 중에 오류가 발생하였습니다.\n이 오류가 계속 발생할시에는 개발자에게 DM을 주십시오.', QMessageBox.Yes)
-    
+        errSaveFile = QMessageBox.critical(self, '오류가 발생하였습니다.', '파일을 저장하는 중에 오류가 발생하였습니다.\n이 오류가 계속 발생할시에는 개발자에게 DM을 주십시오.', QMessageBox.y)
+        
+        if errSaveFile == QMessageBox.Yes:
+            self.close()
+
     def loadFile(self):
         print("loading...")
         print("ERROR Occurred!\nidk why it happend. sry about that :(")
-        errSaveFile = QMessageBox.critical(self, '오류가 발생하였습니다.', '파일을 불러오는 중에 오류가 발생하였습니다.\n이 오류가 계속 발생할시에는 개발자에게 DM을 주십시오.', QMessageBox.Yes)
+        errLoadFile = QMessageBox.critical(self, '오류가 발생하였습니다.', '파일을 불러오는 중에 오류가 발생하였습니다.\n이 오류가 계속 발생할시에는 개발자에게 DM을 주십시오.', QMessageBox.y)
+
+        if errLoadFile == QMessageBox.Yes:
+            self.close()
 
 # run
 if __name__ == '__main__':
