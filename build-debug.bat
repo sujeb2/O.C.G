9dd007e85068533e9f4c2c7503c5366324fbca98
songro_ <sdbb2q@gmail.com>
2023-05-05 19:24:18 +0900

    Update build-debug.bat

--- build-debug.bat
+++ build-debug.bat
@@ -4,5 +4,5 @@ echo "[INFO] ctg debug builder v1.0.0"
 echo "[INFO] Updating..."
 pip install -r requirements.txt
 echo "[INFO] Building..."
-pyinstaller customtag.py -w --clean -n ctg --add-data="./src/img/preview/preview-text-template.png;./src/img/preview/" --add-data="./src/icon_normal.png;./src/" --icon=./src/icon_normal.png
+pyinstaller customtag.py --clean -n ctg --add-data="./src/img/preview/preview-text-template.png;./src/img/preview/" --add-data="./src/icon_normal.png;./src/" --icon=./src/icon_normal.png
 pause
